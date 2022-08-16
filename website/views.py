94ae8f780a7cdf412d22f598647e5eb35d23ffe5
xhlar <contacto@xhlar.com>
2022-08-16 14:51:14 -0500

    FIX views.py
    Signed-off-by: xhlar <contacto@xhlar.com>

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import db
-from .models import Cart, Product
+from .models import Product
 
 views = Blueprint('views', __name__)
 
